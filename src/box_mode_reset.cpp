91abb2517c3775deb4f9187e378a16977fccd9b2
Reimund König <reimund@gmx.net>
2021-07-25 14:11:45 +0200

    simplify reset mode

--- src/box_mode_reset.cpp
+++ src/box_mode_reset.cpp
@@ -8,7 +8,7 @@ box::ModeReset::ModeReset(box::Servomanager* box_servomanager,
                                   box::Wait* box_wait_controller) {
     box::ModeReset::box_servomanager = box_servomanager;
     box::ModeReset::box_wait_controller = box_wait_controller;
-    box::ModeReset::run_mode_reset_step  = 0;
+    run_mode_reset_step  = 0;
 }
 
 box::ModeReset::~ModeReset() {
@@ -22,16 +22,12 @@ bool box::ModeReset::run() {
     int speed = 6;
     switch (run_mode_reset_step) {
         case 0:
-            box_servomanager->move_pilot_servo_to_percent(0, speed);
+            box_servomanager->move_pilot_servo_to_percent(100, speed);
             box_servomanager->move_copilot_servo_to_percent(0, speed);
             run_mode_reset_step++;
             return false; // not finished
         case 1:
             speed = random(4) + 3; // Speed = 3-6
-            box_servomanager->move_pilot_servo_to_percent(100, speed);
-            run_mode_reset_step++;
-            return false; // not finished
-        case 2:
             box_servomanager->move_pilot_servo_to_percent(0, speed);
             box_servomanager->move_copilot_servo_to_percent(0, speed);
             if(random(100) > 50) {
@@ -40,12 +36,14 @@ bool box::ModeReset::run() {
             }
             run_mode_reset_step = 0;
             return true; // finished
-        case 3:
+        case 2:
             speed = random(4) + 3; // Speed = 3-6
             box_servomanager->move_pilot_servo_to_percent(100, speed);
+            box_servomanager->move_copilot_servo_to_percent(0, speed);
             run_mode_reset_step++;
             return false; // not finished
-        case 4:
+        case 3:
+            speed = random(4) + 3; // Speed = 3-6
             box_servomanager->move_pilot_servo_to_percent(0, speed);
             box_servomanager->move_copilot_servo_to_percent(0, speed);
             run_mode_reset_step = 0;
