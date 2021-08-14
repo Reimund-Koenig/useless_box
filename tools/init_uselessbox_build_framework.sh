485e4346fe60897daf5b08a8b9da67e61330366d
Reimund König <reimund@gmx.net>
2021-08-14 16:09:51 +0200

    add src to  symlink

--- tools/init_uselessbox_build_framework.sh
+++ tools/init_uselessbox_build_framework.sh
@@ -7,7 +7,7 @@ sudo apt install libgcc-8-dev &&
 sudo apt-get install build-essential &&
 sudo apt-get install g++
 sudo ln -s ~/workspace/useless_box/include/ /usr/share/arduino/libraries/UselessBox
-sudo ln -s ~/workspace/useless_box/src/ /usr/share/arduino/libraries/UselessBox
+sudo ln -s ~/workspace/useless_box/src/ /usr/share/arduino/libraries/UselessBoxSrc
 sudo ln -s ~/workspace/useless_box/src/manager/ /usr/share/arduino/libraries/UselessBoxManager
 sudo ln -s ~/workspace/useless_box/src/modes/ /usr/share/arduino/libraries/UselessBoxModes
 sudo ln -s ~/workspace/useless_box/src/peripheral/ /usr/share/arduino/libraries/UselessBoxPeripheral
